be8d4744f3f8617e208becbc3e05d0dd390db8a1
Antonio Šiljac <siljacantonio@gmail.com>
2025-09-29 21:51:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #region IMPORTS
-
+import os
 
 #endregion
 
@@ -51,16 +51,60 @@ bank_account = {
 #endregion
 
 
-#region FUNCTIONS
+#region MAIN MENU
+def clear_display() -> None:
+    os.system('cls') if os.name == 'nt' else os.system('clear')
 
+def main_menu():
+    while True:
+        clear_display()
+        print('\t== BANK MANAGER ==')
+        print()
+        print('MENU:')
+        print()
+        print('1. OPENING AN ACCOUNT ')
+        print('2. I HAVE AN ACCOUNT ')
+        print('0. EXIT')
+
+        menu_items = input('Ennter a number of functionality you want: ')
+
+        if menu_items.isdigit():
+            return int(menu_items)
+        else: 
+            print('Invalid entry! Please try again. ')
+            input('To make a new selection, press the ENTER key.')
+#endregion
+
+#region FUNCTIONS
+def opening_an_account():
+    pass
+def existing_account():
+    pass
+#endregion
+
+#region KEY FUNCTIONS
+def main():
+    menu_item = main_menu()
+
+    if menu_item == 0:
+        print()
+        print('Thanks for using Bank manager App! ')
+        return
+    elif menu_item == 1:
+        opening_an_account()
+        print()
+        input('Press ENTER to continue!')
+    elif menu_item == 2:
+        existing_account()
+        print()
+        input('Press ENTER to continue!')
+    else:
+        print(menu_item)
+        input()
 #endregion
 
 
 #region MAIN PROGRAM
-
-#endregion
-
-
-#region END PROGRAM
-
+if __name__ == '__main__':
+    main()
 #endregion
